63b811494a5685f8f82af355e366d8b444bc2898
Wahaj404 <wahajmaqsood20@gmail.com>
2022-05-22 17:02:30 +0500

    refactored

--- util.py
+++ util.py
@@ -1,2 +1,14 @@
+from socket import socket
+from time import sleep
+
 PORT = 4015
 CONNECTION = ('localhost', PORT)
+
+
+def send(conn: socket, msg: str, delay: float = 0.0):
+    conn.send(msg.encode())
+    sleep(delay)
+
+
+def recv(conn: socket) -> str:
+    return conn.recv(1024).decode()
